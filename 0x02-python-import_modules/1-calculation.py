c930de2e0e6a41236075520ba3fb8954b101a823
FousseniTraore <foussenitraore0597@gmail.com>
2022-08-26 07:16:20 -0700

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    from calculator_1 import add, sub, mul
+    a = 10
+    b = 5
+    """ Addition function"""
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    """ Substraction function """
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    """ Multiplication function"""
+    print("{} * {} = {}".format(a, b, mul(a, b)))
