96eefdc3704891d5816e5e78cdd61fedc65878af
FousseniTraore <foussenitraore0597@gmail.com>
2022-08-26 07:22:42 -0700

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,3 +9,4 @@ if __name__ = "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     """ Multiplication function"""
     print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
