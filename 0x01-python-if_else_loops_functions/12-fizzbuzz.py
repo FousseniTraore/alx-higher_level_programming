b5f5c6bbd012a93536fd8148ab342055a3ff6dee
FousseniTraore <foussenitraore0597@gmail.com>
2022-09-17 02:21:29 -0700

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 100):
-        if num % 3 == 0 num % 5 == 0:
+        if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz", end=" ")
         elif num % 3 == 0:
             print("Fizz", end=" ")
