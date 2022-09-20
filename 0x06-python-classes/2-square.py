68ebd0667da8e304317c1a121e278402bff9ef5c
FousseniTraore <104927762+FousseniTraore@users.noreply.github.com>
2022-09-20 19:42:02 +0400

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is 2-square class"""
+
+
+class square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """dOC"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
