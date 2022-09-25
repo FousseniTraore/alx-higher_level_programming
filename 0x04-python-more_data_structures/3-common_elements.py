9498e5cb6136ca44d54ec57203c44cd9e9263410
FousseniTraore <104927762+FousseniTraore@users.noreply.github.com>
2022-09-25 15:58:58 +0400

    Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" a function that returns a set of common elements in two sets"""
+
+
+def common_elements(set_1, set_2):
+    """Return commun element"""
+    return set(set_1) & set(set_2)
