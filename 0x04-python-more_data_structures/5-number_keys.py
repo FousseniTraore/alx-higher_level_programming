b5d5d99784626869aa8e263d85a36efe269936f8
FousseniTraore <104927762+FousseniTraore@users.noreply.github.com>
2022-09-25 21:53:43 +0400

    Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+"""A function that returns the number of keys in a dictionary."""
+
+
+def number_keys(a_dictionary):
+    """Return the length of the dictionary"""
+    return(len(a_dictionary)))
