3a7a467ae82ae4931ad370959040098165c139da
FousseniTraore <104927762+FousseniTraore@users.noreply.github.com>
2022-09-27 00:40:00 +0400

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+def inherits_from(obj, a_class):
+    """Document"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
