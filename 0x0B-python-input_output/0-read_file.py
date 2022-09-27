7212d946f92b9285785b3c35da8bcb063e0bf2d1
FousseniTraore <foussenitraore0597@gmail.com>
2022-09-27 11:46:10 -0700

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that read a text file usign with"""
+
+
+
+def read_file(filename=""):
+    """Doc"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
