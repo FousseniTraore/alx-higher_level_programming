61def319fad565664a15b2664fc8f796e9087d07
FousseniTraore <104927762+FousseniTraore@users.noreply.github.com>
2022-10-06 10:09:13 +0400

    Task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """Doc"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
